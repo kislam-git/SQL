ca18090cba083cd01f0970c5016e487baa95b8f6
kislam-git <kislam.pro@gmail.com>
2025-03-20 00:38:38 +0600

    Create generate_full_year_dates.sql

---
+++ Complex-SQL-Queries/Full-Year-Dates-SQL/generate_full_year_dates.sql
@@ -0,0 +1,11 @@
+WITH DateSeries AS (
+    SELECT TRUNC(DATE '2024-01-01') + LEVEL - 1 AS full_date
+    FROM DUAL
+    CONNECT BY LEVEL <= 365
+)
+SELECT 
+    EXTRACT(YEAR FROM full_date) AS year,
+    EXTRACT(MONTH FROM full_date) AS month,
+    EXTRACT(DAY FROM full_date) AS date,
+    TO_CHAR(full_date, 'Day') AS day_name
+FROM DateSeries;
